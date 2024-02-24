fa9a0afabf22a2bde3dad63476f43aa73e26ff04
Alisson Vinícius <almeidaalisson2014@gmail.com>
2024-02-24 18:37:54 -0300

    feat: adicionando endereços nos clientes

--- projeto_pessoal/dump.sql
+++ projeto_pessoal/dump.sql
@@ -53,3 +53,103 @@ MODIFY email VARCHAR(60) NOT NULL,
 ADD CONSTRAINT unique_email UNIQUE (email);
 
 COMMIT;
+
+# CRIAR TABELA DE ESTADOS:
+CREATE TABLE IF NOT EXISTS estado (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    uf CHAR(2) NOT NULL UNIQUE,
+    descricao VARCHAR(50) NOT NULL
+);
+
+# POPULAR A TABELA DE ESTADOS:
+START TRANSACTION;
+INSERT INTO estado (uf, descricao) VALUES
+('AC', 'Acre'),
+('AL', 'Alagoas'),
+('AP', 'Amapá'),
+('AM', 'Amazonas'),
+('BA', 'Bahia'),
+('CE', 'Ceará'),
+('DF', 'Distrito Federal'),
+('ES', 'Espírito Santo'),
+('GO', 'Goiás'),
+('MA', 'Maranhão'),
+('MT', 'Mato Grosso'),
+('MS', 'Mato Grosso do Sul'),
+('MG', 'Minas Gerais'),
+('PA', 'Pará'),
+('PB', 'Paraíba'),
+('PR', 'Paraná'),
+('PE', 'Pernambuco'),
+('PI', 'Piauí'),
+('RJ', 'Rio de Janeiro'),
+('RN', 'Rio Grande do Norte'),
+('RS', 'Rio Grande do Sul'),
+('RO', 'Rondônia'),
+('RR', 'Roraima'),
+('SC', 'Santa Catarina'),
+('SP', 'São Paulo'),
+('SE', 'Sergipe'),
+('TO', 'Tocantins');
+COMMIT;
+
+# CRIAR TABELA DE CIDADES:
+CREATE TABLE IF NOT EXISTS cidade (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL UNIQUE,
+    estado_id INT NOT NULL,
+    FOREIGN KEY (estado_id) REFERENCES estado(id)
+);
+
+# POPULAR TABELA DE CIDADES:
+START TRANSACTION;
+INSERT INTO cidade (nome, estado_id) VALUES 
+('Londrina', 16),
+('Canoas', 21),
+('Colombo', 16),
+('São Paulo', 25),
+('Santana de Parnaíba', 25),
+('Ipatinga', 13),
+('Campo Mourão', 16);
+COMMIT;
+
+# CRIAR A TABELA INICIAL DE ENDEREÇO:
+CREATE TABLE IF NOT EXISTS endereco (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    rua VARCHAR(80) NOT NULL,
+    numero VARCHAR(10) NOT NULL,
+    bairro VARCHAR(80) NOT NULL,
+    cidade_id INT NOT NULL,
+    cep VARCHAR(9) NOT NULL UNIQUE,
+    FOREIGN KEY (cidade_id) REFERENCES cidade(id)
+);
+
+# POPULAR TABELA DE ENDEREÇOS:
+START TRANSACTION;
+INSERT INTO endereco (rua, numero, bairro, cidade_id, cep) VALUES 
+('Rua Professor João Cândido', '150b', 'Centro', 1, '86010-001'),
+('Travessa Dona Leopoldina', '1616', 'Fátima', 2, '92200-590'),
+('Rua Antônio Falavinha', '11', 'São Gabriel', 3, '83406-190'),
+('Rua Lucas Gassel', '18c', 'Vila Guarani (Z Sul)', 4, '04310-090'),
+('Alameda das Roseiras', '109', 'Morada dos Pinheiros (Aldeia da Serra)', 5, '06519-315'),
+('Rua dos Caldeus', '80', 'Canaã', 6, '35164-143'),
+('Rua Getúlio Ribeiro de Carvalho', '150', 'Conjunto Cafezal 1', 1, '86049-160');
+COMMIT;
+
+# ADICIONAR ENDEREÇOS NOS CLIENTES CADASTRADOS:
+START TRANSACTION;
+ALTER TABLE cliente
+ADD COLUMN endereco_id INT NULL;
+
+UPDATE cliente 
+LEFT JOIN endereco e ON cliente.id = e.id
+SET cliente.endereco_id = e.id
+WHERE cliente.id IN(2, 3, 4, 6, 7);
+
+UPDATE cliente SET endereco_id = 1 WHERE id IN(1, 5);
+UPDATE cliente SET endereco_id = 5 WHERE id = 7;
+
+ALTER TABLE cliente
+MODIFY endereco_id INT NOT NULL,
+ADD FOREIGN KEY (endereco_id) REFERENCES endereco(id);
+COMMIT;
--- projeto_pessoal/views/vw_todos_clientes.sql
+++ projeto_pessoal/views/vw_todos_clientes.sql
@@ -1,11 +1,19 @@
 CREATE VIEW vw_todos_clientes AS
 SELECT 
-    id,
-	nome,
-    cpf,
-    GET_GENDER(sexo) AS genero,
-    DATE_FORMAT(data_nascimento, '%d/%m/%Y') AS dataNascimento,
-    IFNULL(telefone, 'Não cadastrado') AS telefone,
-    email
-FROM cliente
+    c.id AS id_cliente,
+	c.nome,
+    c.cpf,
+    GET_GENDER(c.sexo) AS genero,
+    DATE_FORMAT(c.data_nascimento, '%d/%m/%Y') AS dataNascimento,
+    IFNULL(c.telefone, 'Não cadastrado') AS telefone,
+    c.email,
+    e.cep,
+    CONCAT(e.rua, ', ', e.numero) AS logradouro,
+    e.bairro,
+    cd.nome AS cidade,
+    uf.descricao AS estado
+FROM cliente c
+	INNER JOIN endereco e ON c.endereco_id = e.id
+    INNER JOIN cidade cd ON cd.id = e.cidade_id
+    INNER JOIN estado uf ON cd.estado_id = uf.id
 ORDER BY data_nascimento;
