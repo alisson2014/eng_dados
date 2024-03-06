496c1f78c9b0f9900262d02f4200cf1cf3fd3650
Alisson Vinícius <almeidaalisson2014@gmail.com>
2024-03-05 21:33:32 -0300

    fix: corrigindo alguns pontos do trabalho

--- dados-2/exercicios/trabalho.sql
+++ dados-2/exercicios/trabalho.sql
@@ -19,106 +19,98 @@ O mesmo será utilizado para determinar o valor do produto com seu reajuste em u
 Ex: produto X 10 reais em janeiro 2023 e fevereiro o mesmo custa 11; */
 
 DELIMITER $$
-CREATE PROCEDURE CORRECAO_PRODUTO (
-    cod_classificacao VARCHAR(3),
-    percentual DECIMAL(11,2), 
-    descricao VARCHAR(60), 
-    data_inicio DATE, 
-    data_fim DATE
+CREATE OR REPLACE PROCEDURE reajuste (
+    IN descricao VARCHAR(60), 
+    IN percentual DECIMAL(11,2), 
+    IN data_inicio DATE, 
+    IN data_fim DATE,
+    IN cod_classificacao VARCHAR(3)
 )
 BEGIN
     DECLARE cod_exists INT;
 
-    -- Verifica se o produto existe 
+    -- Verifica se existe algum produto com a classificação passada por parâmetro 
     SELECT COUNT(CODIGO) INTO cod_exists FROM produto WHERE CODIGO_CLASSIFICACAO = cod_classificacao;
 
     IF cod_exists = 0 THEN
         SELECT 'Classificação inválida ou inexistente' AS ERRO;
     ELSE
         INSERT INTO correcao_valor_produto (descricao, percentual, dt_inicio, dt_fim, CODIGO_CLASSIFICACAO) 
-        VALUES (descricao, ROUND(percentual / 100, 2), data_inicio, data_fim, cod_classificacao);
+        VALUES (descricao, ROUND(percentual, 2), data_inicio, data_fim, cod_classificacao);
     END IF;
 END $$
 
 DELIMITER ;
 
-CALL CORRECAO_PRODUTO('013', 101.5, 'Aumento março/2024', CURDATE(), '2024-03-31');
-CALL CORRECAO_PRODUTO('020', 98, 'Desconto março/2024', CURDATE(), '2024-03-31');
-CALL CORRECAO_PRODUTO('999', 100.5, 'Aumento março/2024', CURDATE(), '2024-03-31');
-CALL CORRECAO_PRODUTO('001', 97, 'Desconto abril/2023', '2023-04-01', '2024-04-30');
-CALL CORRECAO_PRODUTO('005', 102, 'Aumento janeiro/2023', '2023-01-01', '2024-01-31');
+CALL reajuste('reajuste janeiro 2023', 1.1, '2023-01-01', '2023-01-31', '005');
+CALL reajuste('reajuste fevereiro 2023', 1.2, '2023-02-01', '2023-02-28', '005');
+CALL reajuste('reajuste março 2023', 1.3, '2023-03-01', '2023-03-31', '005');
+CALL reajuste('reajuste março 2024', 0.1, '2024-01-01', '2024-01-31', '005');
+CALL reajuste('reajuste março 2024', 1.1, '2024-03-01', '2024-03-31','005');
 
 /* 2) Criar uma function que retorne o valor do produto atualizado conforme o período da sua venda; */
 
 DELIMITER $$
 
-CREATE OR REPLACE FUNCTION CALCULA_CORRECAO(
-    valor DECIMAL(10, 0), 
-    data_inicial DATE, 
-    data_final DATE,
-    data_venda DATE, 
-    porcentagem DECIMAL(11, 2)
-) RETURNS DECIMAL(11, 2)
+CREATE OR REPLACE FUNCTION valor_ajustado_2(produto INT, venda INT, data_comparacao DATE) 
+RETURNS DECIMAL(11, 2)
 BEGIN
-    IF (data_venda >= data_inicial AND data_venda <= data_final) THEN
-        RETURN ROUND(valor * porcentagem, 2);
-    ELSE
-        RETURN valor;
-    END IF;
+    DECLARE valor_ajustado DECIMAL(11, 2);
+
+    SELECT 
+        CASE
+            WHEN cvp.cod_correcao IS NULL THEN p.VALOR * p.QUANTIDADE
+            ELSE ((p.VALOR * p.QUANTIDADE) * cvp.percentual)
+        END AS 'ajustado' INTO valor_ajustado
+    FROM vendas v 
+        INNER JOIN itens_venda iv ON v.codvenda = iv.codvenda
+        INNER JOIN produto p ON iv.codproduto = p.CODIGO
+        LEFT JOIN correcao_valor_produto cvp ON cvp.CODIGO_CLASSIFICACAO = p.CODIGO_CLASSIFICACAO
+        AND data_comparacao >= cvp.dt_inicio AND data_comparacao <= cvp.dt_fim
+    WHERE p.CODIGO = produto AND v.codvenda = venda;
+
+    RETURN valor_ajustado;
 END $$
 
 DELIMITER ;
 
 /* 3.1) criar  uma view que traga o valor atualizado do produto na data atual; */
 CREATE OR REPLACE VIEW vw_valor_produto_hoje AS
-SELECT 
-	p.DESCRICAO,
-    p.TIPO,
-    p.CODIGO_CLASSIFICACAO,
-    p.UNIDADE,
-    p.QUANTIDADE,
-    p.VALOR,
-    CALCULA_CORRECAO(p.VALOR, cvp.dt_inicio, cvp.dt_fim, CURDATE(), cvp.percentual) AS VALOR_ATUALIZADO,
-    IF(CURDATE() <= cvp.dt_fim AND CURDATE() >= cvp.dt_inicio, cvp.percentual, NULL) AS PORCENTAGEM
-FROM produto p
-	LEFT JOIN correcao_valor_produto cvp ON p.CODIGO_CLASSIFICACAO = cvp.CODIGO_CLASSIFICACAO
-ORDER BY p.CODIGO_CLASSIFICACAO, p.DESCRICAO;
+SELECT	
+	v.codvenda,
+    v.datavenda, 
+    p.*, 
+    p.QUANTIDADE * p.VALOR AS valor_sem_reajuste,
+    valor_ajustado_2(p.CODIGO, v.codvenda, CURDATE()) AS valor_ajustado
+FROM vendas v 
+	INNER JOIN itens_venda iv ON v.codvenda = iv.codvenda
+    INNER JOIN produto p ON iv.codproduto = p.CODIGO
+ORDER BY v.datavenda;
+
 
 /* 3.2) criar uma view das vendas com o valor dos produtos atualizados por período; */
 CREATE OR REPLACE VIEW vw_vendas_periodo AS
-SELECT 
-	p.DESCRICAO,
-    p.TIPO,
-    p.CODIGO_CLASSIFICACAO,
-    p.UNIDADE,
-    p.QUANTIDADE,
-    p.VALOR,
-    CALCULA_CORRECAO(p.VALOR, cvp.dt_inicio, cvp.dt_fim, vd.datavenda, cvp.percentual) AS VALOR_ATUALIZADO,
-    IF(vd.datavenda <= cvp.dt_fim AND vd.datavenda >= cvp.dt_inicio, cvp.percentual, NULL) AS PORCENTAGEM,
-    vd.datavenda
-FROM produto p
-	INNER JOIN itens_venda iv ON p.CODIGO = iv.codproduto
-	INNER JOIN vendas vd ON iv.codvenda = vd.codvenda
-	LEFT JOIN correcao_valor_produto cvp ON p.CODIGO_CLASSIFICACAO = cvp.CODIGO_CLASSIFICACAO
-ORDER BY p.CODIGO_CLASSIFICACAO, p.DESCRICAO;
+SELECT	
+	v.codvenda,
+    v.datavenda, 
+    p.*, 
+    p.QUANTIDADE * p.VALOR AS valor_sem_reajuste,
+    valor_ajustado_2(p.CODIGO, v.codvenda, v.datavenda) AS valor_ajustado
+FROM vendas v 
+	INNER JOIN itens_venda iv ON v.codvenda = iv.codvenda
+    INNER JOIN produto p ON iv.codproduto = p.CODIGO
+ORDER BY v.datavenda;
+
 
 /* 3.3) criar uma view que retorne o valor total da venda com os produtos atualizado; */
 CREATE OR REPLACE VIEW vw_vendas_totais AS
-SELECT 
-	p.DESCRICAO,
-    p.TIPO,
-    p.CODIGO_CLASSIFICACAO,
-    p.UNIDADE,
-    p.QUANTIDADE,
-    p.VALOR,
-    SUM(CALCULA_CORRECAO(p.VALOR, cvp.dt_inicio, cvp.dt_fim, vd.datavenda, cvp.percentual) * p.QUANTIDADE) AS VALOR_VENDA,
-    IF(vd.datavenda <= cvp.dt_fim AND vd.datavenda >= cvp.dt_inicio, cvp.percentual, NULL) AS PORCENTAGEM,
-    vd.datavenda
-FROM produto p
-	INNER JOIN itens_venda iv ON p.CODIGO = iv.codproduto
-	INNER JOIN vendas vd ON iv.codvenda = vd.codvenda
-	LEFT JOIN correcao_valor_produto cvp ON p.CODIGO_CLASSIFICACAO = cvp.CODIGO_CLASSIFICACAO
-GROUP BY vd.codvenda, p.CODIGO
-ORDER BY p.CODIGO_CLASSIFICACAO, vd.datavenda;
+SELECT	
+	v.codvenda,
+    SUM(p.VALOR * p.QUANTIDADE) AS valor_total_sem_reajuste,
+    SUM(valor_ajustado_2(p.CODIGO, v.codvenda, v.datavenda)) AS valor_total_ajustado
+FROM vendas v 
+	INNER JOIN itens_venda iv ON v.codvenda = iv.codvenda
+    INNER JOIN produto p ON iv.codproduto = p.CODIGO
+GROUP BY v.codvenda;
 
 COMMIT;
